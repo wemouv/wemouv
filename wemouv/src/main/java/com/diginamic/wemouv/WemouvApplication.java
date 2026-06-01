828cce3fa8629a2ec1e99d65d87d78515fc82aad
leotex26 <160853319+leotex26@users.noreply.github.com>
2026-06-01 10:41:14 +0200

    premier commit

---
+++ wemouv/src/main/java/com/diginamic/wemouv/WemouvApplication.java
@@ -0,0 +1,13 @@
+package com.diginamic.wemouv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemouvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemouvApplication.class, args);
+	}
+
+}
---
+++ wemouv/src/test/java/com/diginamic/wemouv/WemouvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.wemouv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemouvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
